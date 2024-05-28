c70bbc667b6bbcc96d839ea3796a48d5c7cd9aed
Akash Patil <akashpatil2002@gmail.com>
2024-05-28 18:12:59 +0530

    changes

--- src/components/AssignmentParent.jsx
+++ src/components/AssignmentParent.jsx
@@ -1,13 +1,16 @@
 import {useState, useEffect} from 'react'
 import Assignment from './Assignment';
 
-
 function AssignmentParent(){
 
     const [inputVal, setinputVal] = useState('')
 
     useEffect(()=>{
-
+        fetch("https://sum-server.100xdevs.com/todos")
+        .then(async(res1)=>{
+            const res2 = await res1.json()
+            
+        })
     },[inputVal])
 
     return(
