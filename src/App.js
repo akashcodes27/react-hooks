80b587bfbaebdbba0e2aa676a8347fd92369b141
Akash Patil <akashpatil2002@gmail.com>
2024-05-31 23:19:18 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import UseEffect from "./components/UseEffect";
-import AssignmentParent from "./components/AssignmentParent";
+import Assignment from "./components/Assignment";
 
 function App() {
 
@@ -8,11 +8,28 @@ function App() {
     <>
       <h1>Hey</h1>
       {/* <UseEffect/> */}
-      <AssignmentParent/>
+      <Assignment/>
     </>
   );
 }
 
 export default App;
+
+//hooks are allow functional components in react to use State, they also allow us to implement lifecycle method features like the ones which were used back in the days of class components.
+
+//Lets understand side effects: lets say we are in a race, we dont take pit stops in every lap, we may only take a few pit stops after every couple of laps when certain condition is attainted. Making a pit stop is just like a side effect. Operations like fetching data from backend are examples of side effects. We dont fetch data from backend everytime we render a component. We only fetch data when a certain condition is met.
+
+//useEffect is the hook very popularly which is used to handle side effects
+//lets say we write logic for fetching data inside the useEffect hook.  Everytime we call the function which changes the state, the component re-renders, also causing DOM to render. 
+
+//whenever state updates, it triggers a re-render and hence DOM updates
+
+// Some popular hooks:
+//useEffect
+//useState
+//useMemo
+//useCallback
+//useRef
+//useContext
 
 
--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -3,9 +3,29 @@ import {useState, useEffect} from 'react'
 
 
 function Assignment(){
+
+    const [btnVal, setBtnVal] = useState(0)
+    
+    
+
+
     return(
         <>
+        <button onClick={()=>{
+            setBtnVal(1)
             
+        }}>1</button>
+        <button onClick={()=>{
+            setBtnVal(2)
+        }}>2</button>
+        <button onClick={()=>{
+            setBtnVal(3)
+        }}>3</button>
+        <button onClick={()=>{
+            setBtnVal(4)
+        }}>4</button>
+
+        <TodoFunc id={btnVal}/>
 
 
         </>
@@ -13,3 +33,25 @@ function Assignment(){
 }
 
 export default Assignment 
+
+
+function TodoFunc({id}){
+    const[todo, setTodo] = useState([])
+    useEffect(()=>{
+        fetch(`https://sum-server.100xdevs.com/todo?id=${id}`)
+        .then((res)=>{
+            res.json()
+            .then((response)=>{
+                setTodo(response)
+                // we fetched a specific todo with the mentioned id 
+                console.log(response)
+            })
+        })
+    },[id])
+    // as we can see above, id is our dependency, which means the logic inside useEffect re-renders everytime the id changes 
+    return(
+        <>
+            {id}
+        </>
+    )
+}
